da85f45fa8006034c3a70ee90e9b4fd68cb6f48f
Your NameGrzegorz <chmajg@gmail.com>
2018-04-17 19:01:13 +0200

    Initial commit

---
+++ src/main/java/pl/grzegorzchmaj/easydiet/EasydietApplication.java
@@ -0,0 +1,12 @@
+package pl.grzegorzchmaj.easydiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasydietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasydietApplication.class, args);
+    }
+}
