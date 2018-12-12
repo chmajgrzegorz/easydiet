dcab34dc11661f77255541fa85c7d33ad4e885e5
Grzegorz Chmaj <chmajg@gmail.com>
2018-12-12 11:13:48 +0100

    Removed HtmlToPdf function

--- src/main/java/pl/grzegorzchmaj/easydiet/controlers/ShoppingListController.java
+++ src/main/java/pl/grzegorzchmaj/easydiet/controlers/ShoppingListController.java
@@ -2,16 +2,11 @@ package pl.grzegorzchmaj.easydiet.controlers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import pl.grzegorzchmaj.easydiet.models.entities.Ingredient;
-import pl.grzegorzchmaj.easydiet.models.services.HtmlToPdfParser;
 import pl.grzegorzchmaj.easydiet.models.services.ShoppingListService;
 import pl.grzegorzchmaj.easydiet.models.services.UserInfoService;
-
-import java.util.Map;
 
 @Controller
 public class ShoppingListController {
@@ -35,31 +30,4 @@ public class ShoppingListController {
         return "shoppinglist";
     }
 
-    @PostMapping("/converthtmlshoppinglisttopdfversion")
-    public String convertHtmlShoppingListToPdfVersion(Model model, RedirectAttributes attr){
-        if(!userInfoService.isLogged()){
-            attr.addFlashAttribute("info", "Ta strona jest dostępna tylko dla zalogowanych użytkowników");
-            return "redirect:/login";
-        }
-
-        String inputFile = "http://localhost:8080/createshoppinglist";
-        String outputFile = "C:/Users/chmaj/Desktop/TestPdf.pdf";
-
-        HtmlToPdfParser.generatePDF();
-
-        attr.addFlashAttribute("info", "Lista w formacie pdf została stworzona");
-        return "redirect:/home";
-    }
-
-    @GetMapping("/test")
-    public String test(){
-
-        return "about";
-    }
-
-    @PostMapping("/testpost")
-    @ResponseBody
-    public String testPost(@RequestParam("name") String name, @RequestParam("surname") String surname){
-        return "Formularz dziala pomyslnie" + name + "       " + surname ;
-    }
 }
--- src/main/java/pl/grzegorzchmaj/easydiet/models/services/UserInfoService.java
+++ src/main/java/pl/grzegorzchmaj/easydiet/models/services/UserInfoService.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.grzegorzchmaj.easydiet.models.entities.User;
 
 @Service
